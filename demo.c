8305c3164ee9b3a252131834fb19bf8fac4329fd
Joseph Kaplan <u1364550@umail.utah.edu>
2025-10-15 13:07:46 -0600

    Update demo.c
    Added description of how MIT License would work with this file.

--- demo.c
+++ demo.c
@@ -1,3 +1,6 @@
+/** This code exists so that we can examine how a MIT License affects this work. 
+Since I created this piece of code, if someone were to use a substantial part or all of the code, they would need to reference my name as author and include the original MIT License text.
+**/
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -5,9 +8,9 @@
 // Date Created: 10/15/2025
 
 // Demo file for Understanding Licensing repository
-
 int main() 
 {
 	printf("%s", "This is a message printed when you run this program.");
 	return 0;
 }
+
